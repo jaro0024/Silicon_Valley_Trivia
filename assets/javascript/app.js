3413e051f81455cdc710af1614f981442dbb516c
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-17 10:45:11 -0500

    pseudo code added

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+
+/* Pseudo code:
+ 1) Start button to go to the first question (click event)
+ 2) once start button is clicked, start 30 second timer and show 1 question and 4 options - only one can be clicked (timer)
+ 3) once an answered is clicked, timer stops and evaluate if answer is correct or not (click event and win/lose function)
+ 4) if correct, show page that says correct and a giphy (if / else statement)
+ 5) else if is incorrect, show page that says incorrect, gives the correct answer and a giphy (if / else statement)
+ 6) else, no answer before timer is up, show page that says you ran out of time, gives the correct answer and a giphy (if / else statement)
+ 7) after 5 seconds, go to next question (timer) (for loop)
+ 8) every time it evaluates if the answer is correct, incorrect or not answered, increment the respective variable (++)
+ 9) once the player has gone through all questions, show a screen with the stats:
+  a) correct answers
+  b) incorrect answers
+  c) unanswered questions
+ 10) add a "start over" button that will reset the game to the begiinning without reloading the page */
+
+
+
+// Global variables
+
+
+// function to start game
+
+    
+
+
+
+// Win / lose function:
+
+
+
+
+// Function to restart game when clicking button
+
+
+// Ready function
+
